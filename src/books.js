5af2c64f234e961a6dc76b9c031e48326d7e7849
Aaron Guo <aaronjoshguo@gmail.com>
2021-11-23 19:56:15 +1300

    First Practice Website
    Just learning the basics of React

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX258_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX384_BO1,204,203,200_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/71qWVhqYuOL._AC_UY218_.jpg",
+    title: "The Vanishing Half",
+    author: "Brit Bennett",
+  },
+];
